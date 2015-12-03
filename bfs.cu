542f3ce296d608275ab04f5cc4b64f97791c0a5d
Max Eshleman <Max@wl-dhcp-224-144.Mines.EDU>
2015-12-03 14:14:38 -0700

    fix

--- bfs.cu
+++ bfs.cu
@@ -93,7 +93,7 @@ Node* generateGraph(int nNodes, int maxEdgesPerNode) {
 	srand((unsigned)time(0)); 
 	Node** nodes = new Node*[nNodes];
 	for (int i = 0; i < nNodes; i++) {
-		Node tmp = new Node(i);
+		Node* tmp = new Node(i);
 		nodes[i] = &tmp;
 	}
 
