d7b339fc8beb0af8fe64b4e21a9cc79148d04e71
Max Eshleman <Max@wl-dhcp-224-144.Mines.EDU>
2015-12-03 13:36:23 -0700

    fix

--- bfs.cu
+++ bfs.cu
@@ -120,7 +120,7 @@ Node* generateGraph(int nNodes, int maxEdgesPerNode) {
 	return nodes; 
 }
 
-void exploreChild(Node child, vector<vector<Node>> path, int depth) {
+void exploreChild(Node child, vector< vector<Node> > path, int depth) {
 	vector<Node> currentPath;
 	if (path.size() <= depth) {
 		path.push_back(currentPath);
@@ -132,8 +132,8 @@ void exploreChild(Node child, vector<vector<Node>> path, int depth) {
 	}
 }
 
-vector<vector<Node>> bfs(Node* nodes, int size) {
-	vector<vector<Node> path;
+vector< vector<Node> > bfs(Node* nodes, int size) {
+	vector< vector<Node> > path;
 
 	Node currentNode = nodes[0];
 	vector<Node> firstPath;
