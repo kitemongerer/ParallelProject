58693c7207f2aa699a9dd7648538075cda8c2111
Max Eshleman <Max@Maxs-MacBook-Pro-2.local>
2015-12-04 11:19:59 -0700

    device

--- bfs.cu
+++ bfs.cu
@@ -35,60 +35,14 @@ public:
 	__device__ int parallelSetExplored(int);
 };
 
-/*__global__ void addBase(int *d_array, int *d_size, int *d_base) {
-	int idx = blockIdx.x * TBS + threadIdx.x;
-	if (idx < *d_size && idx >= TBS) {
-		d_array[idx] = d_array[idx] + d_base[blockIdx.x];
-	}
-}
-
-__device__ int scan_warp(int *d_array, int idx) { 
-	int lane = idx % WS;
-
-	int i = 1;
-	while(i < WS && lane >= i) {
-		d_array[idx] = d_array[idx - i] + d_array[idx];
-		i *= 2;
-	}
-
-	return (lane > 0) ? d_array[idx - 1] : 0; 
-}
 
 //Multiplies each element of sparse matrix by the correct vector element and puts the result back in the matrix
-__global__ void allPrefixSums(int *d_array, int *d_size, int *d_base) {
+__global__ void cachedVisitBFS(int *d_graph, int *d_size) {
 	int idx = blockIdx.x * TBS + threadIdx.x;
 	if (idx < *d_size) {
-		int warpId = (idx / WS) % (TBS / WS);
-		int lane = idx % WS;
-		int arrayVal = d_array[idx];
-		int val = scan_warp(d_array, idx);
-
-		__syncthreads();
-		if (lane == WS - 1){
-			d_array[warpId + blockIdx.x * TBS] = d_array[idx];
-		}
-		__syncthreads();
-
-		// If there are more than WS ^ 2 then more than one warp is needed
-		//To calculate bases
-		if (warpId == 0){
-			scan_warp(d_array, idx);
-		}
-		__syncthreads();
-
-		if (warpId > 0){
-			val = val + d_array[warpId + blockIdx.x * TBS - 1];
-		}
-		__syncthreads();
-
-		// Save block base
-		if ((idx % TBS) == TBS - 1) {
-			d_base[blockIdx.x] = val + arrayVal;
-		}
-
-		d_array[idx] = val;
+		vector<Node*> newPath;
 	}
-}*/
+}
 
 Node* generateGraph(int nNodes, int maxEdgesPerNode) {
 	srand((unsigned)time(0)); 
@@ -174,51 +128,36 @@ int main (int argc, char **argv) {
 	int maxEdgesPerNode = atoi(argv[2]);
 
 	Node* nodes = generateGraph(size, maxEdgesPerNode);
-	vector< vector<Node*> > path = bfs(nodes, size);
 
-	for (int i = 0; i < path.size(); i++) {
-		printf("%i - ", i);
-		for (int j = 0; j < path[i].size(); j++) {
-			printf(" %i ", path[i][j]->getValue());
-		}
-		printf("\n");
-	}
-
-	
-	/*int *d_array, *d_base, *d_size;
+	Node* d_graph;
+	int* d_size;
 
 	// Allocate space for device copies
-	cudaMalloc((void **)&d_array, size * sizeof(int));
-	cudaMalloc((void **)&d_base, ceil(((float) size) / TBS) * sizeof(int));
+	cudaMalloc((void **)&d_array, size * sizeof(Node));
 	cudaMalloc((void **)&d_size, sizeof(int));
-	
 
 	// Copy inputs to device
-	cudaMemcpy(d_array, array, size * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_graph, nodes, size * sizeof(Node), cudaMemcpyHostToDevice);
 	cudaMemcpy(d_size, &size, sizeof(int), cudaMemcpyHostToDevice);
+
+	//Synchronouse bfs
+	vector< vector<Node*> > path = bfs(nodes, size);
 
 	cudaEvent_t start;
 	cudaEventCreate(&start);
     cudaEvent_t stop;
     cudaEventCreate(&stop);
 
-	// MAIN COMPUTATION, SEQUENTIAL VERSION
-	result[0] = 0;
-	for (int i = 1; i < size; i++) {
-		result[i] = result[i-1] + array[i-1];
-	}
 
     int gridSz = ceil(((float) size) / TBS);
     // Record the start event
     cudaEventRecord(start, NULL);
 
 	// Launch sparseMatrixMul() kernel on GPU
-	allPrefixSums<<<gridSz, TBS>>>(d_array, d_size, d_base);
+	cachedVisitBFS<<<gridSz, TBS>>>(d_array, d_size, d_base);
 	
 	// Make sure result is finished
 	cudaDeviceSynchronize();
-	
-	calcBase(d_base, size, d_array);
 
 	// Record end event
 	cudaEventRecord(stop, NULL);
@@ -230,10 +169,17 @@ int main (int argc, char **argv) {
 
 	// Copy result back to host
 	int *gpu_result = (int *) malloc(size * sizeof(int));
-	cudaMemcpy(gpu_result, d_array, size * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(gpu_result, d_graph, size * sizeof(int), cudaMemcpyDeviceToHost);
 
 	bool isCorrect = true;
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < path.size(); i++) {
+		printf("%i - ", i);
+		for (int j = 0; j < path[i].size(); j++) {
+			printf(" %i ", path[i][j]->getValue());
+		}
+		printf("\n");
+	}
+	/*for (int i = 0; i < size; i++) {
 		//printf("%i GPU: %i CPU: %i arr: %i\n", i, gpu_result[i], result[i], array[i]);
 		//Print the result if it is wrong
 		if (result[i] != gpu_result[i]) {
@@ -244,7 +190,7 @@ int main (int argc, char **argv) {
 		if(i % TBS == TBS - 1 || i % TBS == 0) {
 			//printf("%i GPU: %i CPU: %i\n", i, gpu_result[i], result[i]);
 		} 
-	}
+	}*/
 
 	if (!isCorrect) {
 		printf("The results do not match\n");
@@ -254,7 +200,7 @@ int main (int argc, char **argv) {
 
 	// Cleanup
 	cudaFree(d_array); 
-	cudaFree(d_size); */
+	cudaFree(d_size); 
 
 	return 0;
 }
