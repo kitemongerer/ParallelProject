6ada2395f3b204a44c3e354f5b62bf173198a6bb
Max Eshleman <Max@wl-dhcp-239-117.Mines.EDU>
2015-12-05 13:45:48 -0700

    device

--- bfs.cu
+++ bfs.cu
@@ -42,13 +42,13 @@ __global__ void exploreWave(int *d_currentWave, Node *d_graph, int *d_waveSize, 
 		printf("%i hey\n", idx);
 		Node** children = d_graph[idx].getChildren();
 		int numChildren = d_graph[idx].getNumChildren();
-		/*for (int i = 0; i < numChildren; i++) {
+		for (int i = 0; i < numChildren; i++) {
 			printf("child: %i\n", children[i]->getValue());
 			if (children[i]->getExplored() == 0) {
 				d_cost[children[i]->getValue()] = d_cost[idx] + 1;
 				d_graph[children[i]->getValue()].parallelSetExplored(1);	
 			}
-		}*/
+		}
 		
 	}
 	
